a86ef8ca067078dd83ef1265db807d8c7646e0c5
Danilo <danilo.colloca91@gmail.com>
2021-03-02 16:50:33 +0100

    milestone1/layoutBase della pagina in scss

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
-echo active;
+
+require_once 'database/database.php';
+
+
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Milestone1-dischi-DC</title>
+    <link rel="stylesheet" href="dist/app.css">
+  </head>
+  <body>
+    <div id="app">
+      <div class="page-wrapper">
+        <div class="side left">
+        </div>
+        <div class="side right">
+        </div>
+        <header>
+        </header>
+        <main>
+        </main>
+        <footer>
+        </footer>
+      </div>
+    </div>
+    
+  </body>
+</html>
