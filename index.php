b977d2c87e1f53e4c54c590fade968dbf82ace75
Danilo <danilo.colloca91@gmail.com>
2021-03-02 17:00:15 +0100

    milestone1/favicon/logo spotify in header

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ require_once 'database/database.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/spotify-favicon.png" type="image/x-icon" size="32x32" >
     <title>Milestone1-dischi-DC</title>
     <link rel="stylesheet" href="dist/app.css">
   </head>
@@ -23,6 +24,11 @@ require_once 'database/database.php';
         <div class="side right">
         </div>
         <header>
+          <nav>
+            <div class="logo-box">
+              <img src="images/spotify-logo.png" alt="">
+            </div>
+          </nav>
         </header>
         <main>
         </main>
