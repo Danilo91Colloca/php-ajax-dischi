1ddfba263abb3dac7760f27772b0ddcaa3eb482f
Danilo <danilo.colloca91@gmail.com>
2021-03-02 15:02:47 +0100

    inizializzazione per eseguire milestone1/ struttura base database

---
+++ database/database.php
@@ -0,0 +1,83 @@
+<?php
+$data = [
+  [
+    'title' =>  'New Jersey',
+    'year'  => '1988',
+    'artist' => 'Bon Jovi',
+    'label' =>  'Mercury Records',
+    'genre' => 'rock',
+    'imgFront' => '../images/newJersey.jpg'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ],
+  [
+    'title' =>  '',
+    'year'  => '',
+    'artist' => '',
+    'label' =>  '',
+    'genre' => '',
+    'imgFront' => '../images/'
+  ]
+];
