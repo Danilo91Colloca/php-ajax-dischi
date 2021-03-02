80af5ce39876aa503883dd4a6175cdc5b1e6dbd3
Danilo <danilo.colloca91@gmail.com>
2021-03-02 15:38:27 +0100

    database completato

--- database/database.php
+++ database/database.php
@@ -5,79 +5,111 @@ $data = [
     'year'  => '1988',
     'artist' => 'Bon Jovi',
     'label' =>  'Mercury Records',
-    'genre' => 'rock',
+    'genre' => 'hard rock',
     'imgFront' => '../images/newJersey.jpg'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Live at Webley \'86',
+    'year'  => '1992',
+    'artist' => 'Queen',
+    'label' =>  'EMI/Parlophone',
+    'genre' => 'rock',
+    'imgFront' => '../images/liveAtWembley86.png'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Ten Summoner\'s Tales',
+    'year'  => '1993',
+    'artist' => 'Sting',
+    'label' =>  'A&M',
+    'genre' => 'pop rock',
+    'imgFront' => '../images/tensSummonersTales.png'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Steve Gadd Band',
+    'year'  => '2018',
+    'artist' => 'Steve Gadd Band',
+    'label' =>  'BFM Jazz',
+    'genre' => 'jazz',
+    'imgFront' => '../images/steveGaddBand.jpeg'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Brave New World',
+    'year'  => '2000',
+    'artist' => 'Iron Maiden',
+    'label' =>  'EMI',
+    'genre' => 'heavy metal',
+    'imgFront' => '../images/braveNewWorld.jpg'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'One More Car, One More Rider',
+    'year'  => '2002',
+    'artist' => 'Eric Clapton',
+    'label' =>  'Duck/Reprise',
+    'genre' => 'blues',
+    'imgFront' => '../images/oneMoreCar.jpg'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'T.N.T.',
+    'year'  => '1975',
+    'artist' => 'AC/DC',
+    'label' =>  'Albert',
+    'genre' => 'hard rick',
+    'imgFront' => '../images/tnt.jpg'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Nevermind',
+    'year'  => '1991',
+    'artist' => 'Nirvana',
+    'label' =>  'BGC',
+    'genre' => 'grunge',
+    'imgFront' => '../images/nevermind.jpg'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Back to Black',
+    'year'  => '2006',
+    'artist' => 'Amy Winehouse',
+    'label' =>  'Island',
+    'genre' => 'neo soul',
+    'imgFront' => '../images/backToBlack.png'
   ],
   [
-    'title' =>  '',
-    'year'  => '',
-    'artist' => '',
-    'label' =>  '',
-    'genre' => '',
-    'imgFront' => '../images/'
+    'title' =>  'Cuz I Love You',
+    'year'  => '2019',
+    'artist' => 'Lizzo',
+    'label' =>  'Atlantic',
+    'genre' => 'soul',
+    'imgFront' => '../images/cuzILoveYou.png'
+  ],
+  [
+    'title' =>  'Black Radio',
+    'year'  => '2012',
+    'artist' => 'Robert Glasper',
+    'label' =>  'Blue Note Records',
+    'genre' => 'R&B',
+    'imgFront' => '../images/blackRadio.jpg'
+  ],
+  [
+    'title' =>  'Texas Flood',
+    'year'  => '1983',
+    'artist' => 'Steve Ray Vaughan',
+    'label' =>  'Epic',
+    'genre' => 'blues',
+    'imgFront' => '../images/texasFlood.jpg'
+  ],
+  [
+    'title' =>  'Tron Legacy',
+    'year'  => '2010',
+    'artist' => 'Daft Punk',
+    'label' =>  'Walt Disney',
+    'genre' => 'film score',
+    'imgFront' => '../images/tronLegacy.jpg'
+  ],
+  [
+    'title' =>  'Nuovo Cinema Paradiso',
+    'year'  => '1988',
+    'artist' => 'Ennio Morricone',
+    'label' =>  'Mercury',
+    'genre' => 'film score',
+    'imgFront' => '../images/nuovoCinemaParadiso.jpeg'
   ]
 ];
