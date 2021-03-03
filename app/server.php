0c33a178f70861e605f278461cfb3ff85d64d049
Danilo <danilo.colloca91@gmail.com>
2021-03-03 22:49:16 +0100

    bonus/generi by select attivato parametro "all"

--- app/server.php
+++ app/server.php
@@ -3,12 +3,13 @@
 require_once __DIR__ . '/../database/database.php';
 
 if(array_key_exists('genre', $_GET) && !empty($_GET['genre'])){
-  $queryKey = strtolower($_GET['genre']);
-  $data = array_filter($data, function ($queryValue) use ($queryKey) {
-    return (strpos(strtolower($queryValue['genre']), $queryKey) !== false);
-  });
-   
+  $query = strtolower($_GET['genre']);
+  $data = array_filter($data, function ($queryValue) use ($query) {
+    return (strpos(strtolower($queryValue['genre']), $query) !== false);
+  });   
 };
+
+
 header('Content-Type: applications/json');
 echo json_encode($data);
 
