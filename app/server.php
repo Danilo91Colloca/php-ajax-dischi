97cdbd95fd9a404b7ee48e6f948a6d6b9df4706e
Danilo <danilo.colloca91@gmail.com>
2021-03-03 11:15:20 +0100

    refactoring del serer e dei document index

--- app/server.php
+++ app/server.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../database/database-miles2.php';
+require_once __DIR__ . '/../database/database.php';
 
-
-
-
+header('Content-Type: applications/json');
+echo json_encode($data);
